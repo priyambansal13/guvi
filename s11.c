ca5b594219a384e82e0cb9de0105c2ea18d8090d
priyambansal13 <bansalpriyam17@gmail.com>
2018-01-03 20:27:02 +0530

    s11.c

---
+++ s11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n>0)
+   {
+     printf("positive");
+   }
+   else if(n<0)
+   {
+     printf("negative") ;
+   }
+   else
+   printf("zero");
+}
