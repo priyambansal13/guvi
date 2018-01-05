93eb267843e8d0eb6fb7605cab914c66e6a60f80
priyambansal13 <bansalpriyam17@gmail.com>
2018-01-05 21:35:09 +0530

    Add files via upload

--- s11.c
+++ s11.c
@@ -1,16 +1,63 @@
 #include<stdio.h>
-void main()
+int main()
 {
-   int n;
-   scanf("%d",&n);
-   if(n>0)
-   {
-     printf("positive");
-   }
-   else if(n<0)
-   {
-     printf("negative") ;
-   }
-   else
-   printf("zero");
+	int n,a[100],i,j,k=0,count=0,t,c[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=1;j<n;j++)
+		{
+			if(a[j-1]>a[j])
+			{
+				t=a[j-1];
+				a[j-1]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	t=0;
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				count++;
+				t++;		
+			}
+		}
+		if(count>0)
+		{
+			c[k]=a[i];
+			k++;
+		}
+		count=0;
+	}
+	if(t>0)
+	{
+	for(i=0;i<k;i++)
+	{
+		for(j=i+1;j<k;j++)
+		{
+			if(c[j]==c[i])
+			{
+				count++;
+			}
+		}
+		if(count==0)
+		{
+		  printf("%d ",c[i]);
+		}
+		count=0;
+	}
+	}
+	else
+	{
+		printf("unique");
+	}
+	return 0;
 }
