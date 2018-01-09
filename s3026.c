be753907b09f88840907e18747ad70dd768b81a8
priyambansal13 <bansalpriyam17@gmail.com>
2018-01-09 21:01:46 +0530

    Add files via upload

---
+++ s3026.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+ 
+int main() {
+   int arr[30], i, j, num, temp;
+ 
+   printf("\nEnter no of elements : ");
+   scanf("%d", &num);
+ 
+   
+   for (i = 0; i < num; i++) {
+      scanf("%d", &arr[i]);
+   }
+ 
+   j = i - 1;   
+   i = 0;       
+ 
+   while (i < j) {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;          
+      j--;          
+   }
+ 
+  
+   printf("\nResult after reversal : ");
+   for (i = 0; i < num; i++) {
+      printf("%d->", arr[i]);
+   }
+ 
+   return (0);
+}
